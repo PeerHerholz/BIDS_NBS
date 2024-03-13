c2782088b6d96bc22b8cec2837209cff8ccbd199
PeerHerholz <herholz.peer@gmail.com>
2024-03-13 12:51:14 +0100

    comment tests to allow workflows to run

--- bidsnbs/tests/test_nbs_file_creation.py
+++ bidsnbs/tests/test_nbs_file_creation.py
@@ -1,12 +1,7 @@
-import os
-from pathlib import Path
+# def test_nbs_file_creation_json_events():
 
+#     # test if nbs events file in json is created correctly
 
-def test_nbs_file_creation_json_events():
+# def test_nbs_file_creation_json_session():
 
-    # test if nbs events file in json is created correctly
-
-def test_nbs_file_creation_json_session():
-
-    # test if nbs session file in json is created correctly
- 
+#     # test if nbs session file in json is created correctly

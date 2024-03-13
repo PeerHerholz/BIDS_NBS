e570bbfe985a1b0a250bb99c50149d72fada96aa
PeerHerholz <herholz.peer@gmail.com>
2024-03-13 12:48:23 +0100

    add draft for test directory

---
+++ bidsnbs/tests/test_nbs_file_creation.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+
+def test_nbs_file_creation_json_events():
+
+    # test if nbs events file in json is created correctly
+
+def test_nbs_file_creation_json_session():
+
+    # test if nbs session file in json is created correctly
+ 
